8f312d4828b0770a739df1ea91b3fe17a1cb6e07
Hamza <hamza01961@gmail.com>
2021-08-19 15:39:40 +0500

    products styling done

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -11,12 +11,29 @@ function Products({products}:productprops) {
         <div className="products-container">
             {
                 products.map(product=>(
-                    <div className="product">
-                        <img src={product.product_image} alt=""/>
+
+                    <a  href={product.product_link} target="_blank" className="product">
+                        <img src={product.product_image?product.product_image:"noimage.jpg"} alt=""/>
                         <span>{product.title}</span>
                         <span>{product.price}</span>
-                        <span>{product.ratings}</span>
-                    </div>
+
+                            {/* rendering stars according to given ratings */}
+                        <div className="product-ratings">
+                            {
+                                
+                            product.ratings?  [...Array(parseInt((product.ratings).charAt(0)))].map(()=>(
+                                    <span className="fa fa-star checked st"></span>
+                                )):null
+
+                            }
+                            {
+                            product.ratings? [...Array(5-parseInt((product.ratings).charAt(0)))].map(()=>(
+                                    <span className="fa fa-star st"></span>
+                                )):null
+                            }
+                        </div>
+                    </a>
+                   
                 ))
             }
         </div>
