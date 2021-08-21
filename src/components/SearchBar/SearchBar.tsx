51a548416e3124f7483ab3eeb565e7b97d82a1be
Hamza <hamza01961@gmail.com>
2021-08-21 14:30:34 +0500

    All Done

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,34 +1,45 @@
 import './SearchBar.css'
-import {useState} from 'react'
+import {useState,useEffect} from 'react'
 import axios from 'axios'
 import IProduct from '../ProductsInterface'
 import Products from '../Products/Products'
 
 function SearchBar() {
-
+    
     const [input,setInput] = useState<string>("")
     const [data,setData] = useState<Array<IProduct>>([])
     const [selectedValue,setSelectedValue] = useState<string>("default")
+    const [loadingData,setLoadingData] = useState<boolean>(false)
+    
+    useEffect(()=>{
+        setLoadingData(false)
+    },[data])
     
     async function handleClick(){
          
-        try{
+        if(input.length>0){
+            
+            setLoadingData(true)
+            
+            try{
 
-            const searchResults = await axios.get('http://localhost:8000/scrap/',{
-                params:input
-            })
+                const searchResults = await axios.get('http://localhost:8000/scrap/',{
+                    params:input
+                })
 
-            setData(searchResults.data)
+                setData(searchResults.data)
+            }
+            catch(e){
+                console.log(e)
+            }
+
+            setInput("")
         }
-        catch(e){
-            console.log(e)
-        }
-        setInput("")
     }
+
     function getSelectValue()
     {
         setSelectedValue(((document.getElementById("dropdown") as HTMLInputElement).value))
-
     }
     
     return (
@@ -37,15 +48,35 @@ function SearchBar() {
             <div className="search-container">
                 <div className="search-bar">
                     <input type="text" value={input} onChange={(e)=>setInput(e.target.value)} placeholder="product name..." id="search"/>
-                    <div className="button-container">
-                        <button className="button" onClick={handleClick}>Search</button>
-                    </div>
+                    
+                    {
+                        loadingData?
+                        <div className="loading">
+                            <i className="fa fa-circle-o-notch fa-spin fa-3x fa-fw"></i>
+                        </div>
+                        
+                        :
+
+                        <div className="button-container">
+                            <button className="button" onClick={handleClick}>Search</button>
+                        </div>
+                    }
+
+                    {
+                        data.length>0?  
+                            
+                            <select className="selectMenu" name="website" id="dropdown" onChange={getSelectValue} >
+                                <option value="default">Sort By</option>
+                                <option value="amazon">Amazon</option>
+                                <option value="daraz">Daraz</option>
+                            </select>
+                        
+                        :
+                        
+                        null
+                    }
+
                 </div>
-                <select name="website" id="dropdown" onChange={getSelectValue} >
-                    <option value="default">Sort By</option>
-                    <option value="amazon">Amazon</option>
-                    <option value="daraz">Daraz</option>
-                </select>
             </div>
 
                 {/* render products when there is some product in array */}
